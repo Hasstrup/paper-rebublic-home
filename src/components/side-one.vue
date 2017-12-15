f4974ab5f07d8862351d1cf5864ca61b3f555427
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2017-12-15 17:43:48 +0100

    built

--- src/components/side-one.vue
+++ src/components/side-one.vue
@@ -5,11 +5,14 @@
          <h6 class='header-tingg'> Paper-Stack</h6>
        </div>
        <div id='header-twitter'>
-         <svg id="i-twitter" viewBox="0 0 64 64" width="32" height="32">
+         <a href='https://twitter.com/getpaperstack'>
+         <svg id="i-twitter" viewBox="0 0 64 64" >
            <path stroke-width="0" fill="#48C9B0" d="M60 16 L54 17 L58 12 L51 14 C42 4 28 15 32 24 C16 24 8 12 8 12 C8 12 2 21 12 28 L6 26 C6 32 10 36 17 38 L10 38 C14 46 21 46 21 46 C21 46 15 51 4 51 C37 67 57 37 54 21 Z" />
          </svg>
+         </a>
      </div>
     </div>
+    <div class='wrapper'>
     <div id='container-ting'>
       <h1 id='span-me'> {{ headerOne }} </h1>
       <h2 id='span-me2'> {{ headerTwo }}</h2>
@@ -23,7 +26,7 @@
          <br/> from the <a id='button-ting' href='/'> Play Store </a>  and <a id='button-ting' href='/'> App Store </a>  now.
        </h6>
      </div>
-
+     </div>
   </div>
 </template>
 
